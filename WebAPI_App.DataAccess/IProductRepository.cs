b48df3eacccc2060627d0648eccda07c8ade38f6
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-07-23 23:57:07 +0300

    Update IProductRepository.cs

--- WebAPI_App.DataAccess/IProductRepository.cs
+++ WebAPI_App.DataAccess/IProductRepository.cs
@@ -1,10 +1,15 @@
+//CRUD
 using WebAPI_App.Entities;
-
 namespace WebAPI_App.DataAccess
 {
     public interface IProductRepository
     {
         List<Product> GetAll();
+        Product? GetById(int id);
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(int id);
+        void Save();
 
     }
 }
