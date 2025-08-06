da41e10cfa022d004283f715ca2bd1612400124c
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-08-06 23:39:10 +0300

    Create ProductService.cs

---
+++ WebAPI_App.Business/ProductService.cs
@@ -0,0 +1,33 @@
+using WebAPI_App.Entities;
+
+namespace WebAPI_App.Business
+{
+    public class ProductService : IProductService
+    {
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
