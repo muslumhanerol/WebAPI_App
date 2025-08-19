d1c1b0f15c1dbb6045bf8d4a3ed47fee3f8fd261
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-08-19 23:27:25 +0300

    add

--- WebAPI_App.WebAPI/Controllers/ProductController.cs
+++ WebAPI_App.WebAPI/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using WebAPI_App.Business;
+using WebAPI_App.Entities;
 
 namespace WebAPI_App.WebAPI.Controllers
 {
@@ -16,10 +17,20 @@ namespace WebAPI_App.WebAPI.Controllers
         [HttpGet]
         //Tüm veriyi getir()=>_service içerisinden GetAll getir.
         public IActionResult GetAll() => Ok(_service.GetAll());
+
+
+        [HttpPost]
+        //Yeni veri ekle
+        public IActionResult Add(Product product)
+        {
+            _service.Add(product);
+            return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+        }
     }
 }
 
 //ok = 200, sorun yok demek.
+//nameof = adı gibi isimler bulmak için kullanılır. Değişken adını değiştirdiğimizde nameof yazmazsak eşleşmediğini anlamayız, nameof verirsek eşleşmediğini anlarız.
 
 
 
